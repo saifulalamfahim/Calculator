afec2e5f48b80872ac3c105a52a4bce3974a1647
saifulalamfahim <fahim12345saiful@gmail.com>
2022-02-07 20:58:47 +0600

    start

---
+++ script.js
@@ -0,0 +1,17 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for(item of buttons){
+    item.addEventListener('click', (e) =>{
+        buttonText = e.target.innerText;
+        console.log('Button text is', buttonText);
+        if(buttonText == 'X'){
+            buttonText = '*';
+            screen.value +=  buttonText;
+        }
+        else if (buttonText == '='){
+
+        }
+      
+    })
+};
